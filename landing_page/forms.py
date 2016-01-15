37448e6cade7a47f6035fe1ac9bfd6b144ef0bfa
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-01-15 14:24:19 +0530

    HTML5 valdation in form

--- landing_page/forms.py
+++ landing_page/forms.py
@@ -1,16 +1,16 @@
 from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email
+from wtforms_html5 import TextField, EmailField, IntegerField
+from wtforms.validators import DataRequired, Email, Length
 
 from .models import UserDetail
 from landing_page import db
 
 
 class SubscribeForm(Form):
-    first_name = StringField(validators=[DataRequired()])
-    last_name = StringField(validators=[DataRequired()])
-    email = StringField(validators=[DataRequired(), Email()])
-    phone_number = StringField(validators=[])
+    first_name = TextField(validators=[DataRequired()])
+    last_name = TextField(validators=[DataRequired()])
+    email = EmailField(validators=[DataRequired(), Email()])
+    phone_number = TextField(validators=[])
 
     def save(self):
         obj = UserDetail(self.first_name, self.last_name, self.email,
